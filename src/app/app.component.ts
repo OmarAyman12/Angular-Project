1181fa78bb7582079c9e15021311ea71cc5ba366
OmarAyman12 <omaraymanahmed@gmail.com>
2025-05-01 13:01:22 +0300

    Added users and header components

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './users/users.component';
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent, UserComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'first-angular-app';
+}
