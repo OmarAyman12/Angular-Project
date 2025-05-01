79a2a2a0d929f4c1e280bfd20251ca222041eee9
OmarAyman12 <omaraymanahmed@gmail.com>
2025-05-01 14:23:43 +0300

    fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './users/users.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent,UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
-=======
-import { HeaderComponent } from './header/header.component';
-import { UserComponent } from './users/users.component';
-@Component({
-  selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
->>>>>>> omar
 })
-export class AppComponent {
-  title = 'first-angular-app';
-}
+
+export class AppComponent {}
